e3891619d57b5796ed437ad2ddf62b66ec52a09a
manega07_ <22512651@student.ciu.edu.tr>
2025-12-20 12:49:03 +0200

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,5 @@
 ------TRESOR'S 5 COMPLEX QUERIES
 -- QUERY 1: Show books with their authors
-
 SELECT 
     b.title AS book_title,
     a.first_name,
@@ -18,21 +17,29 @@ SELECT
 FROM Borrow br
 JOIN Member m ON br.member_id = m.member_id
 JOIN Book b ON br.book_id = b.book_id;
-
--- QUERY 3: Show borrow records handled by staff
-SELECT 
-    s.full_name AS staff_name,
-    m.full_name AS member_name,
-    b.title AS book_title
-FROM Borrow br
-JOIN Staff s ON br.staff_id = s.staff_id
-JOIN Member m ON br.member_id = m.member_id
-JOIN Book b ON br.book_id = b.book_id;
---QUERY 4: Show books with available copies
+--QUERY 3: Show books with available copies
 SELECT title, copies_available
 FROM Book
 WHERE copies_available > 0
 ORDER BY copies_available DESC;
+--QUERY 4: Shows members who returned books after the due date
+SELECT 
+    m.full_name AS member_name,
+    b.title AS book_title,
+    br.return_date,
+    br.due_date
+FROM Borrow br
+JOIN Member m ON br.member_id = m.member_id
+JOIN Book b ON br.book_id = b.book_id
+WHERE br.return_date > br.due_date;
+--QUERY 5:Counts how many books each member borrowed
+SELECT 
+    m.full_name,
+    COUNT(br.borrow_id) AS total_borrows
+FROM Member m
+LEFT JOIN Borrow br ON m.member_id = br.member_id
+GROUP BY m.full_name;
+
 ------------------------------------------------------------------------------------------------------------------------
 ------SULTAN'S 5 COMPLEX QUERIES
 --TYPE HERE
